2afbc6077dcb06662f8c2ad75e48835c66c5765d
Nenba Jonathan <nenbajonathan@protonmail.com>
2021-12-18 23:31:19 +0100

    Set the default navbar and add a little description about the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import Navbar from "./components/navbar/navbar";
+
 function App() {
-  return (
-    <div className="text-3xl font-bold underline">
-      <h2>Welcome to Cameroom Logos</h2>
-    </div>
-  );
+	return (
+		<div>
+			<Navbar />
+			<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+				<div className="max-w-3xl py-4">
+					<h2 className="text-lg mb-2">
+						CameroonLogos is an open source collection of High
+						qualitym pixel perfect Cameroonian Company logos use for
+						free.
+					</h2>
+					<div className="mb-2" >
+						Search through 20+ vector from different contributor.
+					</div>
+					<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+						Contribute on Github
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
