a700656dc2e249eb1075c7c04afd21ec5dfa1631
Nenba Jonathan <jonathan.nenba@propellerhead.com>
2022-06-24 18:07:54 +0100

    fix: fix the typo and compilation error

--- src/componets/logo.js
+++ src/componets/logo.js
@@ -1,21 +1,31 @@
 import React from "react";
+import PropTypes from 'prop-types';
+
 import "./../styles/logo.scss";
 
-const Logo = () => {
+const Logo = (props) => {
   return (
     <div className="logo">
       <div className="logo-image">
         <img src="logo-1.svg" alt="logo" />
         <div className="logo-image__download">
-          <a href="#">Download</a>
+          <a href={props.url}>Download</a>
         </div>
       </div>
       <div className="logo-infos">
-        <a href="#">Cmr Logo</a>
+        <a href={props.url}>Cmr Logo</a>
         <p>Technology</p>
       </div>
     </div>
   );
+};
+
+Logo.propTypes = {
+  url: PropTypes.string
+};
+
+Logo.defaultProps = {
+  url: 'https://cameroonlogos.com'
 };
 
 export default Logo;
